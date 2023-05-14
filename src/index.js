a205072ac76a6a69eca736dd64bd7899cdc21367
Thrashattack <henrique202@gmail.com>
2023-05-14 23:27:59 +0200

    [feat] Create entrypoint

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-unresolved
+import { playAudioFile } from 'audic';
+import { config } from 'dotenv';
+import searchAppointment from './core/flows/searchAppointment.js';
+import setupDriver from './config/setupDriver.js';
+
+config();
+const driver = await setupDriver();
+
+searchAppointment(driver);
+
+console.log('Appointment Found!');
+await playAudioFile('./src/assets/finallyFound.mp3');
