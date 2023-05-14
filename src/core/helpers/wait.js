5f70c6b8f533f6f4fc2b679747950768137f636a
Thrashattack <henrique202@gmail.com>
2023-05-14 23:40:16 +0200

    [fix] Fix wait options declaration

--- src/core/helpers/wait.js
+++ src/core/helpers/wait.js
@@ -1,9 +1,9 @@
 const wait = async (ms) => new Promise((resolve) => { setTimeout(resolve, ms); });
 
 export default {
-  xs: wait(1000),
-  sm: wait(3000),
-  md: wait(5000),
-  lg: wait(7000),
+  xs: () => wait(1000),
+  sm: () => wait(3000),
+  md: () => wait(5000),
+  lg: () => wait(7000),
   custom: wait,
 };
