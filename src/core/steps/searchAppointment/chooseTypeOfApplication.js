c8c15e744cbcb792f9faf27ab8d3b9cfd8ecb015
Thrashattack <henrique2092@gmail.com>
2023-05-15 15:20:16 +0200

    [fix] adapt to recent changes in page

--- src/core/steps/searchAppointment/chooseTypeOfApplication.js
+++ src/core/steps/searchAppointment/chooseTypeOfApplication.js
@@ -1,4 +1,3 @@
-import { nextButtonSelector } from '../../helpers/selectors.js';
 import wait from '../../helpers/wait.js';
 import xpathContains from '../../helpers/xpathContains.js';
 import timeout from '../../helpers/timeout.js';
@@ -6,7 +5,6 @@ import timeout from '../../helpers/timeout.js';
 const chooseTypeOfApplication = async (driver) => {
   const { TYPE_OF_APPLICATION } = process.env;
   const typeOfApplication = xpathContains('label', TYPE_OF_APPLICATION);
-  const nextButton = xpathContains('button', nextButtonSelector);
   await driver.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
   await driver.mouse.move(Math.random() * 1000, Math.random() * 1000);
   await driver.mouse.move(Math.random() * 1000, Math.random() * 1000);
@@ -14,11 +12,7 @@ const chooseTypeOfApplication = async (driver) => {
   await driver.mouse.move(Math.random() * 1000, Math.random() * 1000);
   const typeOfApplicationButton = await driver.getPath(typeOfApplication, timeout());
   await typeOfApplicationButton.click();
-  await wait.sm();
-
-  const nextButtonButton2 = await driver.getPath(nextButton, timeout());
-  await nextButtonButton2.click();
-  await driver.waitForNavigation();
+  await driver.evaluate(() => window.scrollTo(0, 0));
   await wait.lg();
 };
 
